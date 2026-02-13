7a89d3d5d180f781d83332ff88b09233714b230c
heiling <heilingleonbernal@gmail.com>
2026-02-13 14:16:44 -0500

    creacion menu

---
+++ proyectoPython/coordinador.py
@@ -0,0 +1,19 @@
+def menu_Coordinador():
+    while True:
+        print("1.lista campers")
+        print("2.lista trainers")
+        opcion=int(input(":"))
+        if opcion == 1:
+            print("1.listar campers")
+            print("2.editar campers")
+            print("3.volver")
+            if opcion == 2:
+                print("1.añadir trainers")
+                print("2.editar trainers")
+                print("1.listar trainers")
+                print("4.eliminar trainers")
+                
+
+
+
+
---
+++ proyectoPython/proyecto.py
@@ -0,0 +1,40 @@
+import json
+import os
+from coordinador import menu_Coordinador
+
+def cargar_Usuarios():
+    ruta = os.path.join(os.path.dirname(__file__), "cuentas.json")
+
+    if not os.path.exists(ruta):
+        with open(ruta, "w") as archivo:
+            json.dump([], archivo)
+
+    with open(ruta, "r") as archivo:
+        return json.load(archivo)
+
+while True:
+    print("1. inicia sesion")
+    print("2. registrarse")
+    opcion = int(input("seleccione una opcion: "))
+    if opcion == 1:
+        usuarios =  cargar_Usuarios()
+        correo= input ("ingrese su correo: ")
+        password=input("ingrese su contraseña: ")
+        encontrado= False
+        for usuario in usuarios:
+            if usuario["correo"] == correo and usuario["password"] == password:
+                encontrado = True
+                print("haz iniciado sesion")
+                if usuario["rol"] == "coordinador":
+                   menu_Coordinador()
+            break
+        
+        if not encontrado:
+            print("el usuario no fue encontrado")
+        
+
+                   
+
+            
+
+
